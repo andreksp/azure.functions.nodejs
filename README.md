0380f297676e9520887f1122d68628a2b9d9d3a9
Andre <andreksp@gmail.com>
2022-12-09 17:42:47 -0300

    azure functions example

--- README.md
+++ README.md
@@ -10,3 +10,18 @@ docker ps
 docker logs 7815c681d09c
 docker exec -it 7815c681d09c /bin/bash
 docker kill 7815c681d09c
+
+# Login
+az login 
+az account set --subscription "Assinatura do Azure 1"
+
+# Create Resource Group
+az group create --name azurefunctionsnodejsrg --location brazilsouth
+
+
+# Global ACR Name
+$ACR_NAME='azurefunctionsnodejs'  
+
+# Create ACR
+
+az acr create --resource-group azurefunctionsnodejsrg --name $ACR_NAME --sku Standard
--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const express = require('express');
-const app = express();
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
 
-app.get('/', (req, resp) => {
-    resp.send('teste 2');
-});
+    const name = (req.query.name || (req.body && req.body.name));
+    const responseMessage = name
+        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
-app.listen(3000, () => {
-    console.log('teste');
-});
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: responseMessage
+    };
+}
